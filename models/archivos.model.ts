3723676cc7379156511f6106bb941c0c7513a398
marcoantoniodiazdiaz <kingmarkdc@gmail.com>
2021-04-05 14:14:25 -0500

    Correccion en esta rama para que todo pueda funcionar correctamente

--- models/archivos.model.ts
+++ models/archivos.model.ts
@@ -1,29 +1,5 @@
 import mongoose, { Schema, Document } from 'mongoose';
 import { ObjectId } from 'mongodb';
-import { Model, DataTypes } from 'sequelize';
-import sequelize from '../database/database';
-
-export class Archivos extends Model { }
-
-Archivos.init({
-    // Usuario
-    url: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            isUrl: true,
-        }
-    },
-    tipo: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            isUrl: true,
-        }
-    },
-}, {
-    sequelize, modelName: "archivos",
-});
 
 export interface IArchivos extends Document {
     usuario: string
